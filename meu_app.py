e24bb0b43d86b3ffe5955d5d750b29aaaedf2517
albertodias <64615485+albertodias@users.noreply.github.com>
2024-04-10 21:26:10 -0300

    Novas funcionalidades

--- meu_app.py
+++ meu_app.py
@@ -1,14 +1,33 @@
 #######################################
 # Site de Floricultura Rosas & Espinhos
-# Alberto Dias 08/04/2024 as 21:45 hs
+# Alberto Dias 09/04/2024 as 22:45 hs
 # Python 3.12 + Pandas + Streamlit
 #######################################
 
+
 import streamlit as st
+
 
 import pandas as pd
 
+
+from pandas import read_csv
+
+
+from spacy import load
+
+
 st.set_page_config(page_title="Floricultura Rosas & Espinhos")
+
+
+bar = st.sidebar
+
+
+menu = bar.selectbox(
+    "Opções",
+    ["Relatórios","Pedidos","Contatos","Tabela de Preços"]
+)
+
 
 with st.container(border=True, height=110):
     st.subheader("Floricultura Rosas & Espinhos - 30 anos no mercado.", divider="rainbow")
@@ -16,27 +35,30 @@ with st.container(border=True, height=110):
 
 @st.cache_data
 def carregar_vendas():
-    tabela = pd.read_csv("resultados.csv") # carrega a tabela resultados da vendas nos dias
+    tabela = pd.read_csv("resultados.csv")  # carrega a tabela resultados da vendas nos dias
     return tabela
 
 @st.cache_data
 def carregar_produtos():
-    produto = pd.read_csv("produtos.csv") # carrega a tabela de produtos da Floricultura
+    produto  = read_csv("produtos.csv")
+    # produto = pd.read_csv("produtos.csv") # carrega a tabela de produtos da Floricultura
     return produto
 
-with st.container(border=True, height=520):
+
+with st.container(border=True, height=560):
     st.title("Relatório: Ultimas Vendas")
     qtde_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D", "30D"])
     num_dias = int(qtde_dias.replace("D", ""))
     dados = carregar_vendas()
     dados = dados[-num_dias:]
-    st.area_chart(dados, x="Data", y="Vendas") # apresenta os ultimos vendas do arquivo resultados
+    st.area_chart(dados, x="Data", y="Vendas", width=180, height=300)  # apresenta os ultimos vendas do arquivo resultados
 
 
 with st.container(border=True, height=400):
     st.title("Produtos da Floricultura")
     pro = carregar_produtos()
-    st.table(pro) # apresenta a tabela de produtos
+    # st.table(pro) # apresenta a tabela de produtos
+    st.dataframe(pro)  # troquei por esta com mais opcoes
 
 
 with st.container(border=True, height=110):  # Rodapé da Pagina
